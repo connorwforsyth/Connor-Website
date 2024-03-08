8499bdf8f3fb73b0aab773a4b400b4869b2e5ecc
connorforsythco <62790515+connorwforsyth@users.noreply.github.com>
2024-03-09 10:46:17 +1100

    Attemptng posthog via a server action

--- server-actions/actions.ts
+++ server-actions/actions.ts
@@ -3,6 +3,7 @@ import { getIronSession } from "iron-session";
 import { sessionOptions, SessionData, defaultSession } from "@/lib/lib";
 import { cookies } from "next/headers";
 import { revalidatePath } from "next/cache";
+import { posthog } from "posthog-js";
 
 let password = process.env.ACCESS_CODE!;
 
@@ -24,6 +25,7 @@ export const login = async (
   const formName = formData.get("name") as string;
   const formEmail = formData.get("email") as string;
   const formPassword = formData.get("password") as string;
+  posthog.identify(formName, { name: formName, email: formEmail });
 
   if (formPassword !== password) {
     return { error: "Wrong access code." };
