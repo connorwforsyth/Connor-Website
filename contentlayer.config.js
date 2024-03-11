7fc3513e9f341035bbdd7cbed18c574aebaf4a21
connorforsythco <62790515+connorwforsyth@users.noreply.github.com>
2024-03-12 00:12:53 +1100

    updates to projects

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -2,6 +2,7 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
 import remarkGfm from "remark-gfm";
 import rehypePrettyCode from "rehype-pretty-code";
+import remarkSlug from "remark-slug";
 
 /** @type { import('contentlayer/source-files').ComputedFields }*/
 const computedFields = {
@@ -86,25 +87,12 @@ export default makeSource({
   contentDirPath: "content",
   documentTypes: [Writing, Projects],
   mdx: {
-    remarkPlugins: [remarkGfm],
+    remarkPlugins: [remarkSlug, remarkGfm],
     rehypePlugins: [
       [
         rehypePrettyCode,
         {
           theme: "github-dark",
-          // onVisitLine(node) {
-          //   // Prevent lines from collapsing in `display: grid` mode, and allow empty
-          //   // lines to be copy/pasted
-          //   if (node.children.length === 0) {
-          //     node.children = [{ type: "text", value: " " }];
-          //   }
-          // },
-          // onVisitHighlightedLine(node) {
-          //   node.properties.className.push("line--highlighted");
-          // },
-          // onVisitHighlightedWord(node) {
-          //   node.properties.className = ["word--highlighted"];
-          // },
         },
       ],
     ],
