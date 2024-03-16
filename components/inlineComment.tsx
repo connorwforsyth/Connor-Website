2ba9346f04260f635d6fd1e9ce04e822f62c1aa1
Connor Forsyth <connor@connorforsyth.co>
2024-03-17 10:50:19 +1100

    Completing first draft of the post

--- components/inlineComment.tsx
+++ components/inlineComment.tsx
@@ -4,16 +4,19 @@ type comment = {
   children: any;
 };
 
+// lg:bg-zinc-300 lg:dark:bg-zinc-700
 export default function Comment({ text, children }: comment) {
   return (
-    <span>
-      <span className=" lg:bg-zinc-300 lg:dark:bg-zinc-700">{children}</span>
-      <span
-        className={cn(
-          `opacity-70 before:content-['_('] after:content-[')'] lg:absolute  lg:right-[-11.25rem] lg:w-[11rem] lg:border-l lg:border-zinc-500 lg:pl-2.5  lg:text-sm lg:normal-case lg:first-letter:uppercase lg:before:content-none lg:after:content-['.']  xl:right-[-20rem] xl:w-[18rem]`,
-        )}
-      >
-        {text}
+    <span className="lg:cursor-crosshair">
+      <span className="*:hover:lg:border-l-zinc-950 *:hover:dark:lg:border-l-zinc-400 *:transition-all decoration-zinc-500 decoration-[1.5px] underline-offset-4 transition-all hover:decoration-zinc-950 hover:dark:decoration-zinc-400 lg:bg-zinc-300 lg:underline lg:dark:bg-zinc-800 ">
+        {children}
+        <span
+          className={cn(
+            ` before:content-['_('] after:content-[')'] lg:absolute  lg:right-[-11.25rem] lg:w-[11rem] lg:border-l-[1.5px] lg:border-zinc-500 lg:pl-2.5  lg:text-sm lg:normal-case lg:first-letter:uppercase lg:before:content-none lg:after:content-['.']  xl:right-[-20rem] xl:w-[18rem]`,
+          )}
+        >
+          {text}
+        </span>
       </span>
     </span>
   );
