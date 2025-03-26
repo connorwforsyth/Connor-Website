a74d8a378695b9c6404f4308957fb172edb9bf65
connorforsythco <62790515+connorwforsyth@users.noreply.github.com>
2025-03-26 14:01:36 +1100

    Adding a blog.

--- components/FigmaProtoFrame.tsx
+++ components/FigmaProtoFrame.tsx
@@ -4,14 +4,16 @@ type FigmaProtoFrameProps = {
   src: string;
   className?: string;
   image?: boolean;
+  wrapper?: boolean;
 };
 
-export default function FigmaProtoFrame({
+const FigmaProtoFrame = ({
   src,
   className,
   image,
-}: FigmaProtoFrameProps) {
-  return (
+  wrapper = true,
+}: FigmaProtoFrameProps) => {
+  return wrapper ? (
     <div
       className={cn(
         "relative z-10 flex w-full flex-col justify-center rounded-lg from-orange-300  to-orange-500 md:border md:bg-gradient-to-r md:p-24",
@@ -35,5 +37,29 @@ export default function FigmaProtoFrame({
         )}
       </div>
     </div>
+  ) : (
+    <div
+      className={cn(
+        `mx-auto w-full max-w-5xl  rounded-[8px] border border-zinc-300 bg-zinc-300 p-[4px] shadow-md *:rounded-[4px] dark:bg-white dark:bg-opacity-50 md:rounded-[12px] md:border-white md:bg-white md:bg-opacity-50 md:p-[8px] md:*:rounded-[6px]`,
+        className,
+      )}
+    >
+      {image ? (
+        <Image
+          src={src}
+          alt="image"
+          width={1440}
+          height={1024}
+          className="relative aspect-[1440/1024] w-full overflow-clip bg-white"
+        />
+      ) : (
+        <iframe
+          className="relative aspect-[1440/1024] w-full overflow-clip bg-white"
+          src={`${src}&scaling=scale-down-width&hide-ui=1`}
+        />
+      )}
+    </div>
   );
-}
+};
+
+export default FigmaProtoFrame;
